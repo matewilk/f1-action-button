ae7b0e971ef34bca4552b3df5acc675e792ee710
Mat Wilk <matewilk@gmail.com>
2022-01-26 16:42:00 +0000

    (step 5): add data and error react state

--- visualizations/f1-action-button/index.js
+++ visualizations/f1-action-button/index.js
@@ -2,18 +2,28 @@ import React from "react";
 import { Button } from "nr1";
 
 export default class F1ActionButtonVisualization extends React.Component {
+  state = {
+    data: null,
+    error: null,
+  };
+
   onClickHandler = () => {
+    this.setState({ data: null, error: null });
     const { requesturl } = this.props;
 
     console.log(requesturl);
-    
+
     fetch(requesturl)
       .then((res) => (res.ok ? res : Promise.reject(res)))
-      .then((res) => res.json().then((data) => console.log(data)))
-      .catch((error) => console.log(error));
+      .then((res) => res.json().then((data) => this.setState({ data })))
+      .catch((error) => this.setState({ error }));
   };
 
   render() {
+    const { data, error } = this.state;
+    console.log("data", data);
+    console.log("error", error);
+
     return (
       <div>
         <Button onClick={this.onClickHandler}>Request</Button>
